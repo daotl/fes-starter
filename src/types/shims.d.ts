b02b59b3d3ae6511cb833a085e5e42014d811d6e
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-01-08 16:42:06 +0800

    chore: update shims.d.ts

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,7 +1,18 @@
-// shims.d.ts
-declare module '*.vue' {
+/* eslint-disable no-duplicate-imports */
+// declare interface Window {
+//   // extend the window
+// }
+
+// with vite-plugin-md, markdowns can be treat as Vue components
+declare module '*.md' {
   import type { ComponentOptions } from 'vue'
 
   const component: ComponentOptions
   export default component
+}
+
+declare module '*.vue' {
+  import Vue from 'vue'
+
+  export default Vue
 }
