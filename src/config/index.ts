c237801f7c90cfa685671e61ada427ad95d9f7e6
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-12-03 20:16:54 +0800

    feat: add backend API configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,12 @@ export const CONFIG = {
   menus,
   enabledPagePaths,
   homepage: loader.getConfigValue('HOMEPAGE') || 'home',
+  backends: {
+    restApiBaseUrl:
+      loader.getConfigValue('REST_API_BASE_URL') ||
+      `${location.protocol}//${location.host}/api/rest`,
+    wsApiBaseUrl:
+      loader.getConfigValue('WS_API_BASE_URL') ||
+      `${location.protocol}//${location.host}/api/ws`,
+  },
 } as const
