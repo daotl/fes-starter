616acc517ba1bf063473bcfd404cb07457ca5062
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-12-06 14:38:27 +0800

    fix: use relative API URL to support deploying to subpath

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,11 +7,7 @@ export const CONFIG = {
   enabledPagePaths,
   homepage: loader.getConfigValue('HOMEPAGE') || 'home',
   backends: {
-    restApiBaseUrl:
-      loader.getConfigValue('REST_API_BASE_URL') ||
-      `${location.protocol}//${location.host}/api/rest`,
-    wsApiBaseUrl:
-      loader.getConfigValue('WS_API_BASE_URL') ||
-      `${location.protocol}//${location.host}/api/ws`,
+    restApiBaseUrl: loader.getConfigValue('REST_API_BASE_URL') || `api/rest`,
+    wsApiBaseUrl: loader.getConfigValue('WS_API_BASE_URL') || `api/ws`,
   },
 } as const
